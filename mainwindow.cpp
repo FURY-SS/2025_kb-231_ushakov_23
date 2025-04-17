a1f7cee323331e509678aed6ed319a66496ee0e3
Ushakov Vadim <vadimushakov33@gmail.com>
2025-04-17 18:02:55 +0300

    add methods

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,7 +12,127 @@ MainWindow::MainWindow(QWidget* parent) :
     label->setAlignment(Qt::AlignTop);
     ui->scrollArea->setWidget(label.get());
 
+    // Создание меню и действий
+    //createActions();
 
+    // Настройка спинбокса для толщины кисти
+    connect(ui->spinBox, SIGNAL(valueChanged(int)), this, SLOT(setPenWidth(int)));
+
+    // Инициализация переменных
+    instrument = 1;         // По умолчанию выбран карандаш
+    color.setRgb(0, 0, 0);  // Черный цвет по умолчанию
+    widthOfPen = 3;         // Толщина кисти по умолчанию
+    changed = false;        // Флаг изменений
+
+    // Создание нового файла
+    //newFile();
+
+    // Обновление состояния кнопок инструментов
+    repaintButtons();
+}
+
+void MainWindow::pen() {
+    instrument = 1;
+    repaintButtons();
+}
+
+void MainWindow::lastic() {
+    instrument = 2;
+    repaintButtons();
+}
+
+void MainWindow::line() {
+    instrument = 3;
+    repaintButtons();
+}
+
+void MainWindow::ellipse() {
+    instrument = 4;
+    repaintButtons();
+}
+
+void MainWindow::rectangle() {
+    instrument = 5;
+    repaintButtons();
+}
+
+
+void MainWindow::setPenWidth(int width) {
+    widthOfPen = width;
+}
+
+void MainWindow::setColor() {
+
+}
+
+void MainWindow::repaintButtons() {
+    QColor selectedColor(153, 153, 153);
+    QColor defaultColor(255, 255, 255);
+
+    QList<QPushButton*> butts = this->findChildren<QPushButton*>();
+
+    for (int b = 0; b < butts.size(); b++) {
+        if (butts[b]->objectName().contains("_")) {
+            if (butts[b]->objectName() == QString("pushButton_%1").arg(instrument)) {
+                butts[b]->setStyleSheet(
+                    QString("background-color: rgb(%1, %2, %3);")
+                        .arg(selectedColor.red())
+                        .arg(selectedColor.green())
+                        .arg(selectedColor.blue()));
+            }
+            else {
+                butts[b]->setStyleSheet(
+                    QString("background-color: rgb(%1, %2, %3);")
+                        .arg(defaultColor.red())
+                        .arg(defaultColor.green())
+                        .arg(defaultColor.blue()));
+            }
+        }
+    }
+}
+
+
+void MainWindow::mousePressEvent(QMouseEvent* event) {
+
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent* event) {
+
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent* event) {
+
+}
+
+void MainWindow::paintEvent(QPaintEvent* event) {
+
+}
+
+void MainWindow::drawLine() {
+
+}
+
+void MainWindow::drawEllipse() {
+
+}
+
+void MainWindow::drawRectangle() {
+
+}
+
+void MainWindow::open() {
+
+}
+
+bool MainWindow::save() {
+
+}
+
+void MainWindow::loadFile(const QString& fileName) {
+
+}
+
+void MainWindow::closeEvent(QCloseEvent* event) {
 
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -25,6 +25,30 @@ class MainWindow : public QMainWindow
     // Основные переменные класса
     QPixmap img;        // Основное изображение для рисования
     QPixmap imgTmp;     // Временное изображение для предпросмотра фигур
+    bool isPressed;     // Флаг нажатия кнопки мыши
+    QPointF current;    // Текущая позиция курсора (начальная точка)
+    QPointF next;       // Следующая позиция курсора (конечная точка)
+    int instrument;     // Текущий выбранный инструмент (1-карандаш, 2-ластик и т.д.)
+    QColor color;       // Текущий цвет рисования
+    QPainter painter;   // Объект для рисования
+    int widthOfPen;     // Текущая толщина кисти
+    bool changed;       // Флаг изменений (для запроса сохранения)
+
+
+    // Вспомогательные методы
+    void createActions();               // Создание меню и действий
+    void loadFile(const QString &fileName); // Загрузка файла
+    void repaintButtons();              // Обновление состояния кнопок инструментов
+
+
+protected:
+    // Обработчики событий
+    void paintEvent(QPaintEvent *);             // Событие перерисовки
+    void mouseMoveEvent(QMouseEvent *ev);       // Движение мыши
+    void mousePressEvent(QMouseEvent *ev);      // Нажатие кнопки мыши
+    void mouseReleaseEvent(QMouseEvent *ev);    // Отпускание кнопки мыши
+    void closeEvent(QCloseEvent *event);        // Закрытие окна
+
 
 public:
     explicit MainWindow(QWidget *parent = 0); // Конструктор
@@ -34,8 +58,19 @@ public:
 public slots:
     // Слоты для обработки действий
     //void newFile();             // Создать новый файл
-
-
+    void open();                // Открыть файл
+    bool save();                // Сохранить файл
+    void pen();                 // Выбрать карандаш
+    void lastic();              // Выбрать ластик
+    void line();                // Выбрать линию
+    void drawLine();            // Нарисовать линию
+    void ellipse();             // Выбрать эллипс
+    void drawEllipse();         // Нарисовать эллипс
+    void rectangle();           // Выбрать прямоугольник
+    void drawRectangle();       // Нарисовать прямоугольник
+    void setColor();            // Выбрать цвет
+    void setPenWidth(int width);// Установить толщину кисти
 };
+
 
 #endif // MAINWINDOW_H
